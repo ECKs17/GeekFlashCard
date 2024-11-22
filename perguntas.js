373c7a31ec988336fe3e9e1bc55ed7f19bb294af
ECKs17 <wfgvalor17@gmail.com>
2024-11-22 14:31:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Entreterimento',
+    'Quem lançou a música do Hypno?',
+    'Quem lançou a música do Hypno foi o Anirap'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Quantas músicas sairam no drop novo da URT?',
+    'Foram 10 músicas'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Quem o Ivou fez em seu especial Danganronpa?',
+    'Ele fez o Kokichi Oma'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Título da última música do M4?',
+    'SEGA 6.6.6'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Qual foi a música do Shiny_sz no Drop?',
+    'Jack Esqueleto'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Que música do Drop possuí a seguinte frase" É hora de dormir"?',
+    'Jeff the Killer, do ISHIDA'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Quem fez a música da Coraline?',
+    'Isis Vasconcellos'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Quem são os irmãos "sz"?',
+    'Shiny, Shooter e Seven'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Que fez a música da Remina?',
+    'Chrono'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Quem participou do Especial Mastermind?',
+    'Mistery, Daarui, Chrono, Enygma, Purple e Anny'
+)
